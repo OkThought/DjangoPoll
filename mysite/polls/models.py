388292c064e8e034d461fc9a3de218a19cdc78d2
RoyPy <bogush.vano@gmail.com>
2018-05-14 12:24:56 +0700

    Customize admin site view

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,7 +10,12 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return timezone.now() - timezone.timedelta(days=1) <= self.pub_date <= timezone.now()
+        now = timezone.now()
+        return now - timezone.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
